a0dec7b0c1dcf82dab9cfaedb209b1da4b52dd9d
Stefan Blattmann <stefan.blattmann@gmail.com>
2018-10-11 18:08:32 +0200

    added dist

---
+++ dist/precache-manifest.5e218c2dba9b87bc0e83e118ff191fe6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "df99b0ce49e569d602c5",
+    "url": "/css/chunk-6ef410d5.86056a62.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7e86baca69f9271949de",
+    "url": "/js/chunk-vendors.5377fc0d.js"
+  },
+  {
+    "revision": "c7a7590ee379a1dcb7cf",
+    "url": "/js/chunk-a5d0f5f8.a5919f73.js"
+  },
+  {
+    "revision": "df99b0ce49e569d602c5",
+    "url": "/js/chunk-6ef410d5.4250c7dd.js"
+  },
+  {
+    "revision": "d2d7f23d39012c22aa23",
+    "url": "/js/chunk-15fe10c0.4b4d2cab.js"
+  },
+  {
+    "revision": "39626626343e4a0db2a0",
+    "url": "/js/chunk-0ff845ab.0bf234b8.js"
+  },
+  {
+    "revision": "b100762a25518d40df9d",
+    "url": "/js/app.48c75659.js"
+  },
+  {
+    "revision": "340ffbe371aecdce56bb0d39dfbee034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2d7f23d39012c22aa23",
+    "url": "/css/chunk-15fe10c0.b7433508.css"
+  },
+  {
+    "revision": "39626626343e4a0db2a0",
+    "url": "/css/chunk-0ff845ab.67f8d78d.css"
+  },
+  {
+    "revision": "b100762a25518d40df9d",
+    "url": "/css/app.0e433876.css"
+  }
+];
